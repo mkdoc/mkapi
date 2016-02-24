e72954f6ad050984c437a00f52264ca71017a16f
muji <freeformsystems@gmail.com>
2016-02-24 12:02:34 +0800

    Assert on parameters without type.

---
+++ test/fixtures/params.js
@@ -0,0 +1,12 @@
+/**
+ *  @name foobar
+ *
+ *  @param foo A foo parameter.
+ *  @param bar A bar parameter.
+ */
+
+/* jshint ignore:start */
+function foobar(foo, bar){}
+
+module.exports = foobar;
+/* jshint ignore:end */
---
+++ test/spec/params.js
@@ -0,0 +1,25 @@
+var expect = require('chai').expect
+  , fs = require('fs')
+  , parse = require('../../index');
+
+describe('mdapi:', function() {
+
+  it('should print method parameters', function(done) {
+    var output = 'target/usage.md'
+      , expected = '# API\n\n## foobar\n\n```javascript\nfoobar(foo, bar)\n```'
+          + '\n\n* foo: A foo parameter.\n* bar: A bar parameter.\n\n';
+    function complete(err) {
+      if(err) {
+        return done(err); 
+      }
+      var contents = '' + fs.readFileSync(output);
+      expect(contents).to.eql(expected);
+      done(); 
+    }
+    parse(
+      ['test/fixtures/params.js'],
+      {stream: fs.createWriteStream(output)},
+      complete);
+  });
+
+});
