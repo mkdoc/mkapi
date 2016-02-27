59e574f0845cd3b3b0367f27f327398ff00df49e
muji <freeformsystems@gmail.com>
2016-02-27 14:57:22 +0800

    Draft todo implementation.

---
+++ test/fixtures/todo.js
@@ -0,0 +1,14 @@
+/**
+ *  Module description.
+ *
+ *  @module ModuleName
+ *
+ *  @author Author
+ *  @version 1.1.0
+ *  @since 1.0
+ *  @license MIT
+ *
+ *  @todo {INFO} Actual implementation.
+ *  @todo {WARN} Not optimized.
+ *  @todo Assert on implementation.
+ */
