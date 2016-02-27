6d4e42f860902597048731292e53ff56f30a8574
muji <freeformsystems@gmail.com>
2016-02-27 15:46:32 +0800

    Assert on @todo.

---
+++ test/spec/todo.js
@@ -0,0 +1,25 @@
+var expect = require('chai').expect
+  , fs = require('fs')
+  , parse = require('../../index');
+
+describe('mdapi:', function() {
+
+  it('should print @todo', function(done) {
+    var output = 'target/todo.md'
+      , expected = '' + fs.readFileSync(
+          'test/fixtures/expect/todo.md');
+    function complete(err) {
+      if(err) {
+        return done(err); 
+      }
+      var contents = '' + fs.readFileSync(output);
+      expect(contents).to.eql(expected);
+      done(); 
+    }
+    parse(
+      ['test/fixtures/todo.js'],
+      {stream: fs.createWriteStream(output)},
+      complete);
+  });
+
+});
